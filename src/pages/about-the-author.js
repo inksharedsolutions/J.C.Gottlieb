ca32ce155346d231eac319313f7590eadee9eaef
inksharedsolutions <webdev.inkshared@gmail.com>
2021-02-18 01:19:38 +0800

    Content Update

--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -40,45 +40,27 @@ const ATB = (props)=>{
                             id="author">
 
                             <p>
-                                J.C. Gottlieb was born on January 23, 1948, the second of seven children born to a FrenchCanadian/Irish Catholic couple, in Manchester, NH. Her parents were working class people
-                                whose priorities included sending their children to Catholic schools and giving them a strong
-                                Catholic background. Money was never superfluous in the home, but there was always a little
-                                set aside for emergencies, such as an illness or a sudden repair.
+                                J.C. Gottlieb was born on January 23, 1948, the second of seven children born to a French Canadian/Irish Catholic couple, in Manchester, NH. Her parents were working class people whose priorities included sending their children to Catholic schools and giving them a strong Catholic background. Money was never superfluous in the home, but there was always a little set aside for emergencies, such as an illness or a sudden repair.
+                            </p>
+                            
+                            <p>
+                                J.C. had strong connections with both of her grandmothers as well as numerous aunts, uncles, and cousins. Her Memere taught her to sing hymns to Mary in French at the age of three, accompanying her on the accordion. J.C. was mesmerized by her grandmother's fingers pushing all the correct buttons (without looking) to produce the different sounds on the accordion.
                             </p>
 
                             <p>
-                                The children attended French Catholic elementary schools, and it was in sixth grade religion
-                                class that J.C. learned her initials were the same at Jesus’s. (Upon entering the classroom, she
-                                saw her initials on the board and thought she was in trouble. Sister went straight into her
-                                religion lesson and pointed to the initials, referring to Jesus Christ. Phew!) When the children
-                                were later moved to an all English-speaking school, J.C. found it almost blasphemous to pray
-                                in English, the language used on the street.
+                                The children attended French Catholic elementary schools, and it was in sixth grade religion class that J.C. learned her initials were the same at Jesus's. (Upon entering the classroom, she saw her initials on the board and thought she was in trouble. Sister went straight into her religion lesson and pointed to the initials, referring to Jesus Christ. Phew!) When the children were later moved to an all English-speaking school, J.C. found it almost blasphemous to pray in English, the language used on the street.
                             </p>
 
                             <p>
-                                At Saint Joseph’s High School for Girls, J.C. took the college-preparatory track and was wellprepared to start college upon graduation in 1966. Having attended mostly bi-lingual
-                                elementary schools, J.C. had had very little science education before high school, and when
-                                she started in Biology, she was excited about the new world she was learning about. When one
-                                of her favorite teachers suggested that she might have a calling to be a nun, she innocently
-                                replied, “I used to think so too, but now that I know about babies, I want to get married and
-                                have some myself.”
+                                At Saint Joseph's High school for Girls, J.C. took the college-preparatory track and was well prepared to start college upon graduation in 1966. Having attended mostly bi-lingual elementary schools, J.C. had very little science education before high school, and when she started in Biology, she was excited about the new world she was learning about. When one of her favorite teachers suggested that she might have a calling to be a nun, she innocently replied, "I used to think so too, but now that I know about babies, I want to get married and have some myself."
                             </p>
 
                             <p>
-                                During the last semester of her senior year, J.C. and certain other classmates were offered an
-                                opportunity to attend a class at the local college on Saturday mornings. J.C. chose Creative
-                                Writing and enjoyed it thoroughly. Her professor encouraged her to look into a career as a
-                                writer.
+                                During the last semester of her senior year, J.C. and certain other classmates were offered an opportunity to attend a class at the local college on Saturday mornings. J.C. chose Creative Writing and enjoyed it thoroughly. Her professor encouraged her to look into a career as a writer.
                             </p>
 
                             <p>
-                                At college, J.C. took German to fulfill the language requirement for a B.A. Because the
-                                University offered a Junior Year Abroad program, she decided to major in German. The year in
-                                Marburg, Germany, was a growing-up experience in many ways. Besides becoming fluent in
-                                the language, J.C. learned to live independently. Her many letters home were an opportunity to
-                                practice her descriptive writing skills, because life in Germany was very different from life in the
-                                United States. Upon her return home, her older sister and husband encouraged her to continue
-                                writing, because they had enjoyed her letters.
+                                At college, J.C. took German to fulfill the language requirement for a B.A. Because the University offered a Junior Year Abroad program, she decided to major in German. THe year in Marburg, Germany, was a growing-up experience in many ways. Besides becoming fluent in the language, J.C. learned to live independently. Her many letters home were an opportunity to practice her descriptive writing skills, because life in Germany was very different from life in the United States. Upon her return home, her older sister and husband encouraged her to continue writing, because they had enjoyed her letters.
                             </p>
 
                             <p>
